174d37b867263ec6919c3a95cf89ece44ce3d190
ghulamali17 <ghulamali47092@gmail.com>
2024-11-06 09:31:28 +0500

    Added HTML and CSS for color generator project

--- project.js
+++ project.js
@@ -1,24 +1,24 @@
-function colorGen(){
-    let color="0123456789ABCDEF"
-    let hashh="#"
-    for(let i=0; i<6;i++){
-        hashh+=Math.floor(Math.random()*10)
+function colorGen() {
+    let color = "0123456789ABCDEF";
+    let hashh = "#";
+    for (let i = 0; i < 6; i++) {
+        hashh += Math.floor(Math.random() * 16).toString(16);
     }
     return hashh;
 }
 
-let start=document.getElementById("start");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
 let intervalId;
-start.addEventListener('click', ()=>{
- intervalId= setInterval(changBgColor,1000)
-   function changBgColor(){
-    document.body.style.backgroundColor =colorGen();
-   }
-   
-})
 
-let stop=document.getElementById("stop");
-stop.addEventListener('click', ()=>{
-   clearInterval(intervalId);
-   
+function changBgColor() {
+    document.body.style.backgroundColor = colorGen();
+}
+
+start.addEventListener('click', () => {
+    intervalId = setInterval(changBgColor, 1000);
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(intervalId);
 });
